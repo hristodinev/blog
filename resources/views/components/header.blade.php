e381353a1282dad5caff6c7034885acc69d0268f
Hristo Dinev <arm_104@abv.bg>
2022-03-16 13:04:44 +0200

    Fix the nav spacing in the header and add Post model and the migration for the post.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,17 +1,25 @@
-<section class="px-4 py-4 bg-white sticky top-0 z-50">
-    <nav class="flex justify-between"> 
-        <div class="px-3 py-2">
+<section class="px-4 py-2 bg-white sticky top-0 z-50">
+    <nav class="flex border-b-2 border-black-200 items-center"> 
+        <div class="flex-none w-20 h-14 px-3 py-2">
             <h2 class="font-bold uppercase">
                 <img class="h-8" src="{{url('/img/dot-logo.png')}}" alt="The logo" srcset="">
             </h2>
         </div>
-        <div class="items-center py-3">
-            <a class="font-semibold text-sm uppercase ml-4" href="/">Home</a>
-            <a class="font-semibold text-sm uppercase ml-4" href="/">About</a>
-            <a class="font-semibold text-sm uppercase ml-4" href="/">Contact</a>
-            <a class="font-semibold text-sm uppercase ml-4" href="/">Login</a>
-            <a class="font-semibold text-sm uppercase ml-4 bg-red-600 text-white px-3 py-3" href="#">Subscribe</a>
+        <div class="flex mx-auto w-60 items-center py-3">
+            <a class="font-semibold text-sm uppercase" href="/">Home</a>
+            <a class="font-semibold text-sm uppercase ml-4" href="#">About</a>
+            <a class="font-semibold text-sm uppercase ml-4" href="#">Contact</a>
+            <a class="font-semibold text-sm uppercase ml-4" href="#">Login</a>
+        </div>
+        <div class="flex w-32 py-4 px-4">
+            <a class="font-semibold text-sm uppercase ml-4 bg-red-600 text-white px-2 py-2" href="#">Subscribe</a>
         </div> 
     </nav>
-
+    <header class="mt-10 text-center">
+            <div class="max-w-4xl mx-auto">
+                <h1 class="text-4xl font-semibold">
+                    Latest from <span class="text-red-500">the blog</span>
+                </h1>
+            </div>
+    </header>
 </section>
