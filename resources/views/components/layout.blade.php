bad08021e0017b0822ed32faa1d3f337e6a733b2
Hristo Dinev <arm_104@abv.bg>
2022-03-22 13:00:14 +0200

    Minor changes to the design

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -13,7 +13,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 </head>
-<body class="bg-gray-200">
+<body>
     <x-header/>
     {{$slot}}
 <script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
 <header class="mt-10 text-center">
             <div class="max-w-4xl mx-auto">
                 <h1 class="text-4xl font-semibold">
-                    Latest from <span class="text-red-500">the blog</span>
+                  <span class="text-red-500">#</span>  Latest from <span class="text-red-500">the blog</span>
                 </h1>
             </div>
 </header>
