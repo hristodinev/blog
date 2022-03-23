6e062e72119978f3c9353c68e7495b6a3b7dbdab
Hristo Dinev <arm_104@abv.bg>
2022-03-23 11:30:36 +0200

    Add factories for testing purposes and relationship between user and posts. The slug in categories migration is unique.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id'=>User::factory(),
+            'category_id'=>Category::factory(),
+            'slug'=>$this->faker->slug,
+            'title'=>$this->faker->sentence,
+            'intro'=>$this->faker->paragraph,
+            'body'=>$this->faker->paragraphs(3, true)
+
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $categories= Category::factory()->create(
+            [
+                'name'=>'Business', 
+                'slug'=> 'business'
+            ]
+            );
+         
+         Post::factory(10)->create();
+         
     }
 }
