5036811f743ba18904868329b25fbe4df0c1936a
Hristo Dinev <arm_104@abv.bg>
2022-03-26 12:27:41 +0200

    Make some improvements to search functionality. Use eloquent scope for the search

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,14 @@ class Post extends Model
     protected $fillable = ['category_id', 'title', 'intro', 'slug', 'body'];
 
 
+    public function scopeFilter($query, array $filters){
+
+        $query->when($filters['search'] ?? false, function($query, $search){
+            $query->where('title', 'like', '%'.$search.'%')
+            ->orWhere('body', 'like', '%'.$search.'%');
+        });
+
+    }
 
     //Relationships
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
@@ -16,26 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    
-    $posts= Post::latest();
-    
-
-    if(request('search')){
-        $posts->where('title', 'like', '%'.request('search').'%');
-    }
-
-    return view('posts', [
-        'posts'=>$posts->get(),
-        'categories'=>Category::all()
-    ]);
-});
-
-Route::get('posts/{post:slug}', function(Post $post){
-    return view('post', [
-        'post'=>$post
-    ]);
-});
+Route::get('/', [PostController::class, 'index']); 
+ 
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
 Route::get('categories/{category:slug}', function(Category $category){
     return view('posts_cat', [
