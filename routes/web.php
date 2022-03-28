b79220ae72ed30389740dde82c2e5e43254a626d
Hristo Dinev <arm_104@abv.bg>
2022-03-28 12:22:23 +0300

    Add Breeze for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index']); 
  
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
