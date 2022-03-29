aa4d9249a4044359970a8a464fbe250a0a3faf45
Hristo Dinev <arm_104@abv.bg>
2022-03-29 10:25:16 +0300

    Add some harcoded comments under the post.

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,18 @@
+<article class="flex bg-gray-100 rounded-xl">
+                <div class="px-3 py-3 shrink-0">
+                    <img class="rounded-xl flex space-x-4" src="https://i.pravatar.cc/60" alt="" srcset="">
+                </div>
+                <div class="py-3">
+                    <header>
+                        <h3 class="font-bold">Hristo Dinev</h3>
+                        <p class="text-xs">
+                            <time>Posted 8 ago</time>
+                        </p>
+                    </header>
+                    <p class="mt-5">
+                    Laravel is a web application framework with expressive, elegant syntax. A web framework provides a structure and starting point for creating your application, allowing you to focus on creating something amazing while we sweat the details.
+
+Laravel strives to provide an amazing developer experience while providing powerful features such as thorough dependency injection, an expressive database abstraction layer, queues and scheduled jobs, unit and integration testing, and more.
+                    </p>
+                </div>
+            </article>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-
+<main class="mt-12 px-3 py-5">
     <article class="mt-7 max-w-5xl mx-auto border border-black-200 rounded-lg shadow-lg shadow-gray-500/50">
         <img class="mb-5 rounded-lg shadow-lg shadow-gray-500/50" src="https://picsum.photos/1024/300" alt="" srcset="">
         <h1 class="flex font-bold text-4xl px-3 py-5 mt-5">{{$post->title}}</h1>
@@ -18,5 +18,11 @@
         <div class="flex content-center py-5 px-3">
             <a href="{{route('home')}}" class="bg-red-500 font-semibold text-white text-sm shadow-lg shadow-gray-500/50 px-4 py-2 rounded-xl hover:bg-red-600 focus:outline-none focus:ring focus:ring-red-500">Back</a>
         </div>
+        <section class="mt-10 space-y-6">
+            <x-post-comment />
+            <x-post-comment />
+            <x-post-comment />
+         </section>
     </article>
+</main>      
 </x-layout>
